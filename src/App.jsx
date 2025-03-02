71d341f15fd4ba95294d7a937fc1ecbae462bdc7
Kamran <mkamran4786@gmail.com>
2025-03-02 09:09:44 +0500

    - design footer on home page

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,27 @@ import { ReactComponent as FeaturesSVG } from './assets/images/features.svg';
 import { ReactComponent as Toronto } from './assets/images/toronto.svg';
 import { ReactComponent as MapSVG } from './assets/images/map.svg';
 import { ReactComponent as LocationSVG } from './assets/icons/location.svg';
+import { ReactComponent as PhoneIcon } from './assets/icons/phone.svg';
+import { ReactComponent as SocialMediaIcons } from './assets/images/social-media-icons.svg';
 import Testimonials from './components/Testimonials';
 import StepsCard from './components/StepsCard';
 import greenCircleGif from './assets/images/green-circle.gif';
 
+const navigationLinks = [
+    { label: 'Home', url: '#' },
+    { label: 'About Us', url: '#' },
+    { label: 'Owners', url: '#' },
+    { label: 'Tenants', url: '#' },
+    { label: 'Properties', url: '#' }
+];
+
+const otherLinks = [
+    { label: 'Online Medications', url: '#' },
+    { label: 'Download App', url: '#' },
+    { label: 'Start Treatment', url: '#' },
+    { label: 'Online Urgent Care', url: '#' },
+    { label: 'FAQ', url: '#' }
+];
 function App() {
     return (
         <>
@@ -23,16 +40,9 @@ function App() {
                             >
                                 <div className="text-[61.28px] font-cabinet font-semibold text-white">LOGO</div>
                                 <div className="flex items-center gap-10 text-white text-lg">
-                                    <a href="/" className="font-semibold">Home</a>
-                                    <a href="/">About Us</a>
-                                    <a href="/">Owners</a>
-                                    <a href="/">Tenants</a>
-                                    <a href="/">Properties</a>
+                                    {navigationLinks.map((link) => <a href={link.url}>{link.label}</a>)}
                                 </div>
-                                <Button onClick={() => {
-                                }}
-                                >
-                                    {' '}
+                                <Button onClick={() => {}}>
                                     Start Treatment
                                 </Button>
                             </div>
@@ -203,14 +213,15 @@ function App() {
                         <h3 className="text-themeColor text-2xl font-medium mb-2">STATES WE</h3>
                         <h2 className="text-[52px] font-medium text-gray-800">Currently Serve</h2>
                         <p className="w-[70%] text-center text-[20px] text-customBlack">
-                            We believe everyone should have access to treatment and care. That’s why we’re teaming up with providers
+                            We believe everyone should have access to treatment and care. That’s why we’re teaming up
+                            with providers
                             all over the country. Click on a green state to see the insurance companies
                             we currently accept and to learn more about online Suboxone doctors in your area.
                         </p>
                     </div>
 
                     <div className="mt-5 grid grid-cols-[40%_60%] gap-10">
-                        <div className="flex flex-col gap-4 mt-5">
+                        <div className="flex flex-col gap-3 mt-4">
                             <Toronto />
                             <div className="text-[31.56px] font-semibold">
                                 Toronto
@@ -220,10 +231,88 @@ function App() {
                                 <span className="text-[#00927C] text-[18.41px] font-semibold">Ottawa St. and Homer Waston Blvd.</span>
                             </div>
                         </div>
-                        <div className="w-fit h-fit max-h-[521px] overflow-y-hidden">
+                        <div className="w-fit max-h-[521px] overflow-y-hidden">
                             <MapSVG />
                         </div>
                     </div>
+                </div>
+            </div>
+
+            <div className="h-[502px] mt-32 relative get-app-block-wrapper">
+                <div className="absolute h-[502px] inset-0 get-app-block opacity-80" />
+                <div className="flex justify-center items-center z-1 absolute w-full h-full">
+                    <div className="max-w-[868px] flex flex-col items-center gap-5">
+                        <div className="text-lg font-medium">SECURE ACCESS TO YOUR MEDICATION</div>
+                        <div className="text-4xl font-medium">Get Your Meds Online Today</div>
+                        <div className="text-[20px] text-black">
+                            We have partnered with RX-Outreach to provide mail order medications at a fraction of
+                            the price you may find in your local pharmacy. Meet your chronic illness provider
+                            online today and get your medication delivered as quickly as 24 hours later.
+                        </div>
+                        <Button
+                            variant="black"
+                            onClick={() => {
+                            }}
+                            icon={<ArrowIcon />}
+                        >
+                            Get The App
+                        </Button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="mt-16 w-full flex flex-col items-center">
+                <div className="w-[80%]">
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+                        <div>
+                            <div className="text-themeColor text-[76.52px] font-medium">LOGO</div>
+                            <div className="mt-5 text-customBlack text-lg max-w-[459px]">
+                                Experience the convenience of Recovery Delivered, your trusted source for
+                                online medication delivery. We bring the pharmacy to your doorstep,
+                                ensuring fast, discreet, and reliable service. Whether you&apos;re managing a
+                                chronic condition or need a one-time prescription filled, we make the process simple and
+                                hassle-free
+                            </div>
+                        </div>
+                        <div className="flex justify-between">
+                            <div>
+                                <div className="text-themeColor text-2xl">Quick Links</div>
+                                <div className="mt-3 flex flex-col gap-5">
+                                    {navigationLinks.map((link) => (
+                                        <a className="text-customBlack text-lg" href={link.url}>
+                                            {link.label}
+                                        </a>
+                                    ))}
+                                </div>
+                            </div>
+                            <div>
+                                <div className="text-themeColor text-2xl">Other Links</div>
+                                <div className="mt-3 flex flex-col gap-5">
+                                    {otherLinks.map((link) => (
+                                        <a className="text-customBlack text-lg" href={link.url}>
+                                            {link.label}
+                                        </a>
+                                    ))}
+                                </div>
+                            </div>
+                            <div>
+                                <div className="ml-10 flex flex-col gap-7">
+                                    <div className="text-themeColor text-2xl">Contact</div>
+                                    <div className="flex items-center gap-2 text-customBlack text-lg">
+                                        <PhoneIcon />
+                                        <span className="text-customBlack">1-866-757-9868</span>
+                                    </div>
+                                    <div className="text-themeColor text-2xl">Social Media</div>
+                                    <SocialMediaIcons />
+                                </div>
+                                <p className="mt-10 text-sm text-customBlack">Designed and developed by Ropstam</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="mt-32 w-full h-[1px] bg-[#C6C6C6]" />
+                    <p className="my-5 text-sm text-customBlack">©2024 – recovery Delivered | All Right Reserved</p>
+
                 </div>
             </div>
         </>
